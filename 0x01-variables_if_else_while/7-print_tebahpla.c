97a12717b239b313425da1e8bdabb76bb3414a69
Felipe Serna <feserna86@gmail.com>
2020-02-07 03:53:10 +0000

    Smile in the mirror rev0

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char C;
+for (C = 'z' ; C >= 'a' ; C--)
+putchar(C);
+putchar('\n');
+return (0);
+}
