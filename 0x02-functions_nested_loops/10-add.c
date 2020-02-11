ff98cb767208e0b466ca90b91606a289b7739afe
Felipe Serna <feserna86@gmail.com>
2020-02-11 23:11:52 +0000

    Addition rev0

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - adds two integers
+ * @a: Letter to check
+ * @b: Letter to check
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
