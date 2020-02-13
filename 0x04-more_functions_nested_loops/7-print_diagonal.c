77cc44bf0db5a13c35168732014da3dea433e6dc
Felipe Serna <feserna86@gmail.com>
2020-02-13 21:55:32 +0000

    Diagonally parked rev0

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: Letter to check
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
