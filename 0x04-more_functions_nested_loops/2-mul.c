3ec44188af74a441232f58fd80a1e15e01d1b73b
Felipe Serna <feserna86@gmail.com>
2020-02-13 13:26:33 +0000

    multiplication rev0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - function that multiplies two integers
+ * @a: Letter to check
+ * @b: Letter to check
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
