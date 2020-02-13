f83c5035f96e34ad1d7c7ae1462cd1e9e9d032b8
Felipe Serna <feserna86@gmail.com>
2020-02-13 23:40:55 +0000

    Square rev0

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_square - prints a square
+ * @size: Letter to check
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
