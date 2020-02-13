727fa797183953285c041b93d0d17b5ff1e56459
Felipe Serna <feserna86@gmail.com>
2020-02-13 19:57:47 +0000

    Straight line rev0

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,13 +6,13 @@
  */
 void print_line(int n)
 {
+	int j;
+
 	if (n > 0)
+
+	for (j = 0; j <= n; j++)
 	{
 		_putchar(95);
-		_putchar('\n');
 	}
-	else
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
