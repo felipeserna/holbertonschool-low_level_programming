a75f7639c2150f5d5ad06d1db4e9b0a1a3df19fd
Felipe Serna <feserna86@gmail.com>
2020-02-14 02:08:06 +0000

    Fizz Buzz rev1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,24 +10,30 @@ int main(void)
 
 	for (number = 1; number <= 100; number++)
 	{
+		if (number != 100)
+		{
+			if (number % 5 == 0 && number % 3 == 0)
+			printf("FizzBuzz ");
+			else if (number % 3 == 0 && number % 5 != 0)
+			printf("Fizz ");
+			else if (number % 5 == 0 && number % 3 != 0)
+			printf("Buzz ");
+			else
+			printf("%d ", number);
 
-	if (number % 5 == 0 && number % 3 == 0)
-	{
-		printf("FizzBuzz ");
-	}
-	else if (number % 3 == 0 && number % 5 != 0)
-	{
-		printf("Fizz ");
-	}
-	else if (number % 5 == 0 && number % 3 != 0)
-	{
-		printf("Buzz ");
-	}
-	else
-	{
-		printf("%d ", number);
-	}
-	}
+		}
+		else
+		{
+			if (number % 5 == 0 && number % 3 == 0)
+			printf("FizzBuzz");
+			else if (number % 3 == 0 && number % 5 != 0)
+			printf("Fizz");
+			else if (number % 5 == 0 && number % 3 != 0)
+			printf("Buzz");
+			else
+			printf("%d", number);
+		}
+}
 	printf("\n");
 	return (0);
 }
