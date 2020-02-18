9621ffb2fc866e495c2ae17d4583daa6784694c4
Felipe Serna <feserna86@gmail.com>
2020-02-18 13:45:00 +0000

    Swap horses rev0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: Letter to check
+ * @b: Letter to check
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
