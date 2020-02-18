ebcee7dc3f9ebf75e482ea1fee34a37903272248
Felipe Serna <feserna86@gmail.com>
2020-02-18 13:21:40 +0000

    98 Battery rev0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * void reset_to_98 - takes a pointer to an int as parameter and updates the value it points to 98
+ * @n: Letter to check
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
