ad02fde325af49a74b1457ee04864eec1c3b398b
Felipe Serna <feserna86@gmail.com>
2020-02-20 21:06:42 +0000

    strcmp rev1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,20 +8,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int result;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			result = s1[i] - s2[i];
-			return (result);
-		}
-		else
-		{
-			result = 0;
-			return (result);
+			return (s1[i] - s2[i]);
 		}
 	}
-					return (0);
+	return (0);
 }
