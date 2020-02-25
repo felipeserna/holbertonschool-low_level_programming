2845a1cf5b21177771db61e65a9a677c04010ffd
Felipe Serna <feserna86@gmail.com>
2020-02-25 03:30:16 +0000

    strstr rev1

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,28 +9,21 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 	int j;
+	int a;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++, i++)
+		a = i;
+		for (j = 0; (haystack[i] == needle[j]) &&
+			     (haystack[i] != '\0') &&
+			     (needle[i] != '\0'); j++, i++)
 		{
-			if (haystack[i] == needle[j])
-			{
-				break;
-			}
-			else if (haystack[i] != '\0')
-			{
-				break;
-			}
-			else if (needle[i] != '\0')
-			{
-				break;
-			}
+			continue;
 		}
-		if (haystack[i] == needle[j])
+		if (needle[j] == '\0')
 		{
-			return (&haystack[i]);
+			return (haystack = haystack + a);
 		}
 	}
-	return ('\0');
+	return (0);
 }
