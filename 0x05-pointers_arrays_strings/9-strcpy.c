6957bedb56f4de18209218b26188091ee8711f45
Felipe Serna <feserna86@gmail.com>
2020-02-28 19:27:06 +0000

    strcpy rev0

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _strcpy - copies the string
+ * @dest: string to check
+ * @src: string to check
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = 'a';
+	*src = 'b';
+	return (0);
+}
