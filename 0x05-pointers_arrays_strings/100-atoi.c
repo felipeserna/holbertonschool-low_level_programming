dbfcb91da2c2a85b9062ede20ada9d88154253d1
Felipe Serna <feserna86@gmail.com>
2020-02-29 20:53:12 +0000

    atoi rev0

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to check
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	*s = 'a';
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/#7-leet.c#
@@ -0,0 +1,8 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337
+ * @str: array of type char
+ * Return: str
+ */
+char *leet(char *str)
+{
