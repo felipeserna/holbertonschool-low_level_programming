ea02fbe3bf10370945230057819ed47bdd1c09e7
Felipe Serna <feserna86@gmail.com>
2020-03-01 20:45:37 +0000

    Multiplies two numbers rev0

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
- * main - prints the multiplication of two numbers
- * @s: Letter to check
- * Return: nothing
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
  */
-void main()
+int main(int argc, char *argv[])
 {
+	int i, mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
 }
