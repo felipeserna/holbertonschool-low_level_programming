6f42bcab607978c734109194e199e55f8595a9ca
Felipe Serna <feserna86@gmail.com>
2020-03-01 20:10:41 +0000

    Number of arguments rev0

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
- * main - prints the number of arguments passed into it
- * @s: Letter to check
- * Return: nothing
+ * main - prints the number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
  */
-void main()
+int main(int argc, char *argv[])
 {
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
 }
