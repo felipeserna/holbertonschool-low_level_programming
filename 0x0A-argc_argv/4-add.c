89dfe5b7a03f000000fd20204498276879be437e
Felipe Serna <feserna86@gmail.com>
2020-03-01 21:43:48 +0000

    Adds positive numbers rev0

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
- * main - prints the sum of positive numbers
- * @s: Letter to check
- * Return: nothing
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
  */
-void main()
+int main(int argc, char *argv[])
 {
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum = sum + atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	else if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	return (0);
 }
