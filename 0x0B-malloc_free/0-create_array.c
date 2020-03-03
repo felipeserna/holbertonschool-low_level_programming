7b21985d3dfd6fb491cc1aa0a7710913888e1a8d
Felipe Serna <feserna86@gmail.com>
2020-03-03 21:29:05 +0000

    Array of chars rev0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars, and initializes it with
+ * a specific char
+ * @c: array of characters
+ * @size: unsigned integer
+ * Return: 0 if successful
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	s = malloc((size + 1) * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	s[i] = '\0';
+	return (s);
+}
