b6e163e03613cebca08634bf3af5ccc2900ca623
Felipe Serna <feserna86@gmail.com>
2020-03-03 22:12:07 +0000

    Array of chars rev3

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,10 +15,23 @@ char *create_array(unsigned int size, char c)
 
 	s = malloc((size) * sizeof(char));
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	else
+	{
 	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
 	}
 	s[i] = '\0';
 	return (s);
+	}
 }
