11537c553b4f160ecf20b3590181d34945db14e9
Felipe Serna <feserna86@gmail.com>
2020-03-04 04:12:27 +0000

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: width
+ * @height: height
+ * If NULL is passed, treat it as an empty string
+ * Return: pointer pointing to a new space which contains s1,
+ * followed by s2, and null. NULL on failure
+ */
+void free_grid(int **grid, int height)
+{
+	(void) grid;
+	(void) height;
+}
