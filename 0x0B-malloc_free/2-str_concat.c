565c453d5436434a4ec31f5476b4bb12469ab7df
Felipe Serna <feserna86@gmail.com>
2020-03-04 02:37:11 +0000

    Concatenates two strings rev2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,12 +37,12 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < len1; i++)
 	{
 		s3[i] = s1[i];
 	}
 
-	for (j = 0; s2[j] != '\0'; j++, i++)
+	for (j = 0; j < len2 ; j++, i++)
 	{
 		s3[i] = s2[j];
 	}
