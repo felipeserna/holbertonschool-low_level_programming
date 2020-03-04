ccbe4c67ca21eb2f8d164bf7d302b6cf1e76a53d
Felipe Serna <feserna86@gmail.com>
2020-03-04 04:32:41 +0000

    100-strtow.c

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strtow - splits a string into words
+ * @str: argument count
+ * If NULL is passed, treat it as an empty string
+ * Return: pointer pointing to a new space which contains s1,
+ * followed by s2, and null. NULL on failure
+ */
+char **strtow(char *str)
+{
+	(void) str;
+	return (0);
+}
