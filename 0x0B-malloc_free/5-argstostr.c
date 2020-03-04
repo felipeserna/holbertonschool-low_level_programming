932734179e570b2e338d6710655fde2e4a1f087e
Felipe Serna <feserna86@gmail.com>
2020-03-04 04:18:49 +0000

    5-argstostr.c

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argument count
+ * @av: argument vector
+ * If NULL is passed, treat it as an empty string
+ * Return: pointer pointing to a new space which contains s1,
+ * followed by s2, and null. NULL on failure
+ */
+char *argstostr(int ac, char **av)
+{
+	(void) ac;
+	(void) av;
+	return (0);
+}
