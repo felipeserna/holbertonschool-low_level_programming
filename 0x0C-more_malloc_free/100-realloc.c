8742654dfa4f3ff6ade6d49421cc1965eea490fb
Felipe Serna <feserna86@gmail.com>
2020-03-06 01:46:32 +0000

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: minimum number
+ * @old_size: maximum number
+ * @new_size:
+ * Return: pointer to the array, NULL if malloc fails
+ * or if min is greater than max
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	(void) ptr;
+	(void) old_size;
+	(void) new_size;
+	return (0);
+}
