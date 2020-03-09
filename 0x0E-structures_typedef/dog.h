28e3afb61dab80421705481d2008b2949842f0a4
Felipe Serna <feserna86@gmail.com>
2020-03-09 23:22:17 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+int _putchar(char c);
+/**
+ * struct dog - task 0
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: task 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}; /*structure for task 0 */
+#endif /* DOG_H */
