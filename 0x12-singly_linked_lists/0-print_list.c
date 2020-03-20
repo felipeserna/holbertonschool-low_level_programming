788978c5432d5af9820575de888e2fc82753aa60
Felipe Serna <feserna86@gmail.com>
2020-03-20 01:03:40 +0000

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: struct dog
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	char *str;
+
+	if (str == NULL)
+		printf([0] (nil));
+
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
