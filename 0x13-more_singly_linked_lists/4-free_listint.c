c4aa8ccf2b0f60740f9f1c9faf6378b0c5130b70
Felipe Serna <feserna86@gmail.com>
2020-03-23 23:00:28 +0000

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list.
+ * @head: first element of the listint_t singly linked list.
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	if (head == NULL)
+		return;
+
+	node = head;
+
+	while (head != NULL)
+	{
+		node = head->next;
+		free(head);
+		head = node;
+	}
+	free(head);
+}
