02ba78103b97c1456d307ad271755bcde16f0be2
Felipe Serna <feserna86@gmail.com>
2020-03-24 22:11:09 +0000

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,7 +17,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	j = head;
 
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index && j != NULL; i++)
 	{
 		if (i == index)
 			return (j);
