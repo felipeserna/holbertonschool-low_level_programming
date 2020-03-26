e99cce7e3d539431183da76b1d131396bc0d482b
Felipe Serna <feserna86@gmail.com>
2020-03-26 22:58:36 +0000

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: is pointing to a string of 0 and 1 chars.
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i;
+	unsigned int dec = 0;
+
+	if (!b)
+	{
+		return (0);
+	}
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			dec = (dec * 2) + (b[i] - '0');
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	return (dec);
+}
