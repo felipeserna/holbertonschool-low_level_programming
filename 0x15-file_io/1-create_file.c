547bc3e4024f01426f7b76a019dfdb2820bbf488
Felipe Serna <feserna86@gmail.com>
2020-03-31 23:31:11 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,17 +17,19 @@ int create_file(const char *filename, char *text_content)
 	if (o == -1)
 		return (-1);
 
-	if (text_content)
-	{
-		while (text_content[r] != '\0')
-			r++;
+	if (!text_content)
+		return (1);
 
-		w = write(o, text_content, r);
+	for (r = 0; text_content[r] != '\0'; r++)
+	{
 	}
 
-	if (w == -1 || w != r)
+	w = write(o, text_content, r);
+
+	if (w == -1)
 	{
-		return (-1);
+		close(o);
+		return (1);
 	}
 
 	close(o);
