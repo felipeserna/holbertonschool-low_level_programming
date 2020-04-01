351ccb64ae765d1634b1bbe58bbc96730ba60a99
Felipe Serna <feserna86@gmail.com>
2020-04-01 00:41:35 +0000

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - copies the content of a file to another file
+ * @argc: argc
+ * @argv: argv
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	(void) argv;
+	return (0);
+}
