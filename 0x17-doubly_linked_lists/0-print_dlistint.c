bcbe6237082d063c8b296c6c72d48c57a6b69566
Felipe Serna <feserna86@gmail.com>
2020-05-02 17:41:14 +0000

    task 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: list structure
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
