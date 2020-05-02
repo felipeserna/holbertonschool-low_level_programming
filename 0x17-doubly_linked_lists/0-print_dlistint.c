e16446f181f718710bbff29be0283295c81f8467
Felipe Serna <feserna86@gmail.com>
2020-05-02 20:57:03 +0000

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,11 +8,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t length = 0;
 
-	while (h != NULL)
+	do
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		length += 1;
-	}
+	}while (h != NULL);
 	return (length);
 }
