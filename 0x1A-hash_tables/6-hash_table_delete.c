83e46aa75989b9a0edb16a3892fea8fc889bc430
Felipe Serna <feserna86@gmail.com>
2020-05-29 08:17:05 +0000

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *tmp = NULL, *aux = NULL;
+
+	if (ht && ht->array)
+	{
+		while (index < ht->size)
+		{
+			tmp = ht->array[index];
+			while (tmp)
+			{
+				aux = tmp->next;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+				tmp = aux;
+			}
+			index++;
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
