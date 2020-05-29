d3610f20c2b54405f6185fd7d93c92db9245086c
Felipe Serna <feserna86@gmail.com>
2020-05-29 15:04:13 +0000

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,7 +7,8 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int index = 0;
-	hash_node_t *tmp = NULL, *aux = NULL;
+	hash_node_t *tmp = NULL;
+	hash_node_t *aux = NULL;
 
 	if (ht && ht->array)
 	{
@@ -22,7 +23,7 @@ void hash_table_delete(hash_table_t *ht)
 				free(tmp);
 				tmp = aux;
 			}
-			index++;
+			index += 1;
 		}
 		free(ht->array);
 		free(ht);
