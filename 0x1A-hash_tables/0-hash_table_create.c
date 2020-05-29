fe8df27e4ae1eccda0d76822c71a66f2b51e9e0f
Felipe Serna <feserna86@gmail.com>
2020-05-29 00:27:15 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,26 +6,26 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *htable = NULL;
+	hash_table_t *hash_table = NULL;
 	/* Create **array with htable->size = size */
 
 	if (size  > 0)
 	{/*create htable with malloc*/
-		htable = malloc(sizeof(hash_table_t));
-		if (!htable)
+		hash_table = malloc(sizeof(hash_table_t));
+		if (!hash_table)
 			return (NULL);
 		/* Assignament size and array */
-		htable->size = size;
-		htable->array = calloc(size, sizeof(hash_node_t *));
-		if (!htable->array)
+		hash_table->size = size;
+		hash_table->array = calloc(size, sizeof(hash_node_t *));
+		if (!hash_table->array)
 		{
-			free(htable);
+			free(hash_table);
 			return (NULL);
 		}
 		/*for (int i = 0; i < size; i++) clean each cell of this array*/
 		/*htable->array[i] = NULL;*/
 
-		return (htable);
+		return (hash_table);
 	}
 	return (NULL);
 
