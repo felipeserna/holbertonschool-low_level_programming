c970bf952422a237fc8a1c2ceeca86271e6d4de5
Felipe Serna <feserna86@gmail.com>
2020-08-27 18:32:45 +0000

    for changed to while

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -27,12 +27,14 @@ int jump_search(int *array, size_t size, int value)
 
 	printf("Value found between indexes [%d] and [%d]\n", i, step);
 
-	for (; i < (int)size; i++)
+	while (i < (int)size)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
 			return (i);
+
+		i += 1;
 	}
 	return (-1);
 }
