3d167ba0103e6fa3ec5ea6f16c2e806f58eda013
Felipe Serna <feserna86@gmail.com>
2020-08-27 18:46:33 +0000

    100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,6 +13,9 @@ int jump_search(int *array, size_t size, int value)
 	int i = 0;
 	int step = sqrt((int)size);
 
+	if (!array)
+		return (-1);
+
 	while (i < (int)size)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
@@ -27,14 +30,12 @@ int jump_search(int *array, size_t size, int value)
 
 	printf("Value found between indexes [%d] and [%d]\n", i, step);
 
-	while (i < (int)size)
+	for (; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
 			return (i);
-
-		i++;
 	}
 	return (-1);
 }
